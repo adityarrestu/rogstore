65844e4d0a4d59e975f67e1945ac6f712739231d
Aditya Restu Adji <adit.restu77@gmail.com>
2022-07-12 22:45:35 +0700

    new logo in about page

--- page/about.php
+++ page/about.php
@@ -1,11 +1,11 @@
 <div class="container" style="min-height: 80vh;">
   <div class="row d-flex align-items-center justify-content-center my-4">
     <div class="col">
-      <div class="d-flex align-items-center justify-content-center mb-4">
-        <img src="./img/background.jpeg" class="w-75 mx-auto" alt="">
+      <div class="d-flex align-items-center justify-content-center mb-5">
+        <img src="./img/logo-icon.png" class="w-25 mx-auto" alt="">
       </div>
-      <p class="text-white fs-4">Selamat Datang di ROG Store Indonesia</p>
-      <p class="text-white fs-5">Temukan beragam informasi produk dan promo di sini!</p>
+      <p class="text-white text-center fs-4">Selamat Datang di ROG Store Indonesia</p>
+      <p class="text-white text-center fs-5">Temukan beragam informasi produk dan promo di sini!</p>
     </div>
   </div>
 </div>
